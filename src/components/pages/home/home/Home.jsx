f93c4bf24381f36a507b27e5b967a41274163926
muhammadali246397 <muhammadali246397@gmail.com>
2023-10-21 20:46:24 +0600

    setup project

---
+++ src/components/pages/home/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <p className='text-5xl'>this is home page</p>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/components/pages/share/navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            This is navigation bar
+        </div>
+    );
+};
+
+export default Navbar;
